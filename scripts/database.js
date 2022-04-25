24981ef11fe6390b3b5e40d6c3f9ffa90fd8b310
Connor Lopshire <lopshire.connor@gmail.com>
2022-04-25 13:46:07 -0500

    added 3 movies id# 41-43

--- scripts/database.js
+++ scripts/database.js
@@ -1,11 +1,29 @@
 const database = {
 movies: [    {
-        id: "",
-        title: "" ,
-        year:  "",
-        synopsis: "",
-        spoiler: "",
+        id: "41",
+        title: "The Usual Suspects" ,
+        year:  "1995",
+        synopsis: "group of criminals executes heist most of them die",
+        spoiler: "Kevin Spacey did it",
+
+    },
+    {
+        id: "42",
+        title: "The Sixth Sense" ,
+        year:  "1999",
+        synopsis: "Cole is frightened by visitations from those with unresolved problems who appear from the shadows",
+        spoiler: "He sees ghost",
+
+    },
+    {
+        id: "43",
+        title: "The Visit" ,
+        year:  "2015",
+        synopsis:"Kids visit creepy grandparents they've never met",
+        spoiler: "Grandparents were really murdered by escaped mental hospital patients",
 
     }
+   
+
 ]
 }
