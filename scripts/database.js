646767dd91625d6a7006b0f0a999178fc454baea
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-04-25 13:53:43 -0500

    added movies to database

--- scripts/database.js
+++ scripts/database.js
@@ -1,11 +1,76 @@
 const database = {
 movies: [    {
-        id: "",
-        title: "" ,
-        year:  "",
-        synopsis: "",
-        spoiler: "",
+        id: 10,
+        title: "Avengers:  Infinity War" ,
+        year:  "2018",
+        synopsis: "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
+        spoiler: "Spider-Man dies in this one",
 
-    }
+    },
+    {
+        id: 11,
+        title: "Avengers: Endgame",
+        year: "2019",
+        synopsis: "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store.",
+        spoiler: "Spider-Man comes back, but now Iron-Man is dead. :(",
+    
+    },
+    {
+        id: 12,
+        title: "Looper",
+        year: "2012",
+        synopsis: "Joe is classified as a looper, a job in which his employers use time travel to send men from the future to be killed into the past, where Joe can properly dispose of their bodies.",
+        spoiler: "He's been trying to kill himself the whole time!",
+    },
+    {
+        id: 13,
+        title: "Memento",
+        year: "2000",
+        synopsis: "A man with short-term memory loss attempts to track down his wife's murderer.",
+        spoiler: "The one who really killed his wife was Leonard himself!",
+    },
+    {
+        id: 14,
+        title: "Black Swan",
+        year: "2010",
+        synopsis: " A committed dancer struggles to maintain her sanity after winning the lead role in a production of Tchaikovsky's Swan Lake",
+        spoiler: "She doesn't maintain her sanity, this girl is crazy.  She stabs herself in the gut.",
+    },
+    {
+        id: 15,
+        title: "Gone Girl",
+        year: "2014",
+        synopsis: "When his seemingly perfect wife Amy goes missing, philandering Nick becomes the prime suspect.",
+        spoiler: "She faked her own murder and framed her husband",
+    },
+    {
+        id: 16, 
+        title: "Dirty Rotten Scoundrels",
+        year: "1988",
+        synopsis: "Two con men try to settle their rivalry by betting on who can swindle a young American heiress out of fifty thousand dollars first.",
+        spoiler: "She was the one conning them the whole time.",
+    },
+    {
+        id: 17,
+        title: "The Prestige",
+        year: "2006",
+        synopsis: "After a tragic accident, two stage magicians in 1890s London engage in a battle to create the ultimate illusion while sacrificing everything they have to outwit each other.",
+        spoiler: "The secret to the trick was it was two guys the whole time.  Christian Bale was actually twins.",
+    },
+    {
+        id: 18,
+        title: "It's a Wonderful Life",
+        year: "1946",
+        synopsis: "An angel is sent from Heaven to help a desperately frustrated businessman by showing him what life would have been like if he had never existed.",
+        spoiler: "He decides not to kill himeself.",
+
+    },
+    {
+        id: 19,
+        title: "The Sound of Music",
+        year: "1965",
+        synopsis: "A young novitiate is sent by her convent in 1930s Austria to become a governess to the seven children of a widowed naval officer.",
+        spoiler: "The nun decides to marry this dude old enough to be her dad, then the Nazi's come and everyone has to go on a long hike.",
+    },
 ]
 }
