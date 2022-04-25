351a3cc934e5128aa4cc09c98029a742b6abf9f1
Griffin Moyer <moyerga@outlook.com>
2022-04-25 14:01:36 -0500

    adding many movies to database, including all the Shrek documentaries

--- scripts/database.js
+++ scripts/database.js
@@ -1,11 +1,61 @@
 const database = {
 movies: [    {
-        id: "",
-        title: "" ,
-        year:  "",
-        synopsis: "",
-        spoiler: "",
+        id: 30,
+        title: "Big" ,
+        year:  "1988",
+        synopsis: "A little kid turns into Tom Hanks",
+        spoiler: "Tom Hanks turns back into the little kid. Hell yeah."
 
+    },
+    {
+        id: 31,
+        title: "Blade",
+        year:  "1998",
+        synopsis: "Wesley Snipes is half-vampire and is hunting down pure vampires",
+        spoiler: "Head vampire turns into a blood god, but Wesley Snipes kills him. Hell yeah."
+
+    },
+    {
+        id: 32,
+        title: "Men In Black 2" ,
+        year:  "2002",
+        synopsis: "An alien disguised as a lingerie model tries to take over MIB headquarters",
+        spoiler: "Will Smith and Tommy Lee Jones take the building back. Hell yeah."
+    },
+    {
+        id: 33,
+        title: "Goodburger",
+        year:  "1997",
+        synopsis: "A giant burger place called Mondo Burger threatens to put Good Burger out of business... not if Dexter and Ed can help it!",
+        spoiler: "Dexter and Ed find that Mondo Burger is putting illegal chemicals in their burgers to make them bigger. They bring this to the cops, they shut the place down. Good Burger stays in business. Ok, hell yeah. ",
+    },
+    {
+        id: 34,
+        title: "Shrek" ,
+        year:  "2001",
+        synopsis: "Shrek the Ogre doesn't want anyone in his swamp. People start coming into his swamp. In fact, lord Farquaad tries to TAKE his swamp. Shrek's not a fan of that. Shrek cuts a deal with Lord Farquaad. He'll get his swamp back if he rescues Princess Fiona to marry Lord Farquaad.",
+        spoiler: "So, turns out Princess Fiona (who is Cameron Diaz the whole time btw) is an Ogre too? Lord Farquaad gets eaten by a dragon, Shrek and Fiona get married instead and get the swamp back. Hell yeah, you are THRIVING, Shrek!",
+    },
+    {
+        id: 35,
+        title: "Shrek 2" ,
+        year:  "2004",
+        synopsis: "So Shrek and Fiona gotta go meet Fiona's parents. They don't know that they're orges tho! Talk about in-law drama.",
+        spoiler: "Everyone tries to turn back into a human (including the donkey) to be more norm-core and impress Fiona's parents. In the end they decide its not worth it, turn back into their disgusting selves, and sing Livin La Vida Loca. Hell yeah."
+    },
+    {
+        id: 36,
+        title: "Shrek 3" ,
+        year:  "2007",
+        synopsis: "Shrek wants to keep shrekking",
+        spoiler: "Shrek just kept shrekking on. Hell yeah, Shrek.",
+    },
+    {
+        id: 37,
+        title: "Shrek 4" ,
+        year:  "2010",
+        synopsis: "Shrek wanted to shrek till he couldn't shrek anymore",
+        spoiler: "Shrek fully entered shrek mode.",
     }
 ]
 }
