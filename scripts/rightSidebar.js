c9c728afeb73e815f5940d8ae48432683eb961ab
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-04-26 15:10:11 -0500

    added a sidebar

---
+++ scripts/rightSidebar.js
@@ -0,0 +1,11 @@
+export const rightSide = () => {
+    let htmlString = ""
+    htmlString += 
+    `<header>
+    <img src="https://www.fg-a.com/welcome/welcome-smilies.gif" alt="Paris"> <img src="https://www.fg-a.com/aliens/alien-run-1W.gif">
+    <img src="https://imgs.search.brave.com/y357Tudf55eAm6Py_w2Awp_6eYIz2memFv--IGW0XEg/rs:fit:546:614:1/g:ce/aHR0cDovL3dvcmxk/d2lkZWludGVyd2Vi/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAxNy8xMC9iZXN0/LW5pY29sYXMtY2Fn/ZS1waG90b3Nob3Au/anBn" class="nic">
+    <img src ="https://www.fg-a.com/crabs/dancing-crab-animated.gif"><img src="https://www.fg-a.com/furniture/chair-red-office-2018.gif"><img src="https://www.fg-a.com/earth-day/2019-enjoy-the-earth-animation.gif">
+    </header>`
+    return htmlString
+}
+
