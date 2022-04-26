822e3dc85617619e1130db0de873a62c120ab5ff
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-04-26 10:04:48 -0500

    Still added sections but nnow they work

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,18 @@
-<section class="questions"></section>
-<section class="console"></section>
-<div class="sidebar"></div>
-<div class="sidebar"></div>
 
+
+
+
+const mainContainer = document.querySelector(".mainContent")
+
+
+const applicationHTML = `
+<section class="questions">
+whatever we are calling this fun//
+</section>
+<section class="console">
+    whatever we wanna call this one
+</section>
+<div class="sidebar">sidebar me babey</div>
+<div class="sidebar">sidebar also maybe</div>
+`
+mainContainer.innerHTML = applicationHTML
